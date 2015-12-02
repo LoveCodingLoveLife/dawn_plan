3e287a95a67edbe2aea34c9468bdc7d5f80d90a0
YuanChenLi <yclichn@163.com>
2015-12-02 11:41:50 +0800

    ###

---
+++ models/mongodb.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+//var uri = "mongodb://localhost/NodeJS";
+//var uri = "mongodb://01c2ace4-d8d5-4ece-b4f6-c5e8bc34563a:i3BBte-WCJANn_Q-yDT5-w@10.9.27.25:27017/6715d247-df7f-4dbb-9cf2-d4e163285472";
+var uri = "mongodb://admin:admin@ds053877.mongolab.com:53877/yclichn";
+var mongoosedb = mongoose.createConnection(uri);
+module.exports = mongoosedb;
